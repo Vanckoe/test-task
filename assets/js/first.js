3b3c08805645131d621385664eff10429bed08a7
Vanckoe <adil210@mail.ru>
2024-12-17 20:27:54 +0500

    second task

--- assets/js/first.js
+++ assets/js/first.js
@@ -23,6 +23,7 @@ let num = 456;
 let strFromNumber = String(num);
 console.log("Строка из числа:", strFromNumber, "Тип:", typeof strFromNumber);
 
+console.log("--------------");
 // let fruitCount = 5;
 // const fruitExist = false;
 // const warms = null;
@@ -32,7 +33,6 @@ console.log("Строка из числа:", strFromNumber, "Тип:", typeof st
 // } else {
 //   console.log("У нас нет яблок(");
 // }
-
 const fruit = prompt("Какой у вас фрукт в кармане?");
 const iHateApples = [
   "яблоко",
@@ -51,19 +51,50 @@ if (fruit == "") {
   alert("Блин, не люблю яблоко, в конец очереди!");
   console.log("Вас отправили в конец очереди, попробуйте в следующий раз...");
 } else {
-  let num1 = prompt(`Хм интересно... А сколько у вас ${fruit}`);
-  let num2 = prompt(` А сколько вы еще ${fruit} купите?`);
+  let num1 = prompt(`Хм интересно... А сколько у вас ${fruit}?`);
+  let num2 = prompt(`А сколько вы еще ${fruit} купите?`);
+
   num1 = Number(num1);
   num2 = Number(num2);
+
   let fruitSum = num1 + num2;
+
   if (isNaN(fruitSum)) {
     alert("Странный ответ, вожак вас не понял ;/");
     console.log("Ваш бред никто не понял, попробуйте еще раз");
   } else {
-    alert("ура! у нас будет " + fruitSum + " " + fruit);
-    console.log(
-      "Вам нужно доставить " + fruitSum + " " + fruit + " странному вожаку"
-    );
+    if (fruitSum <= 10) {
+      alert(fruitSum + " " + fruit + " - мало, нужно больше!");
+      console.log("Вас отправли в конец очереди");
+    } else {
+      if (fruitSum > 15 && fruitSum < 20) {
+        console.log("У вас достаточно фруктов, чтобы вожак был доволен!");
+      } else if (fruitSum >= 20) {
+        console.log("Очень много! Вожак, наверное, слишком много просит...");
+        alert("ура! у нас будет " + fruitSum + " " + fruit);
+      } else {
+        console.log("Маловато но нормально");
+      }
+
+      if (fruitSum == 10) {
+        console.log("У вас ровно 10 фруктов! О, чудо, магия чисел!");
+      } else if (fruitSum === 10) {
+        console.log("Жесткая проверка! У вас точно 10!");
+      }
+
+      if (fruitSum % 2 === 0 && fruitSum !== 10) {
+        console.log(
+          "Сумма фруктов четная и не равна 10. Суеверный вожак доволен, Всё по плану."
+        );
+      }
+
+      let divisionResult = fruitSum / 3;
+      let remainder = fruitSum % 3;
+      console.log("---------------------");
+      console.log("Результат деления суммы фруктов на 3:", divisionResult);
+      console.log("Остаток от деления на 3:", remainder);
+    }
   }
 }
+
 // что то я заигрался...
