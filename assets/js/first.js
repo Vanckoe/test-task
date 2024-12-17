a15280319ff6631da413f6209d598f7f883b576e
Vanckoe <adil210@mail.ru>
2024-12-17 20:35:31 +0500

    switch case

--- assets/js/first.js
+++ assets/js/first.js
@@ -97,4 +97,34 @@ if (fruit == "") {
   }
 }
 
-// что то я заигрался...
+const month = prompt("Введите номер месяца (от 1 до 12):");
+
+switch (parseInt(month)) {
+  case 1:
+  case 2:
+  case 12:
+    alert("Зима");
+    console.log("Зима");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    alert("Весна");
+    console.log("Весна");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    alert("Лето");
+    console.log("Лето");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    alert("Осень");
+    console.log("Осень");
+    break;
+  default:
+    alert("Ошибка: введите число от 1 до 12.");
+    console.log("Ошибка: введите число от 1 до 12.");
+}
